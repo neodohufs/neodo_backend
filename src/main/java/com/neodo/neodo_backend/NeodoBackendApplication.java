83205a577537453dd908557d5ae34e816d388f5e
geneeuchoi <chyj2108@naver.com>
2025-01-20 20:12:26 +0900

    init commit

---
+++ src/main/java/com/neodo/neodo_backend/NeodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.neodo.neodo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeodoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeodoBackendApplication.class, args);
+    }
+
+}
