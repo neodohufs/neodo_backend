63e38fc3eaf8f541f55d90358d2e7fd66d4d06fe
geneeuchoi <chyj2108@naver.com>
2025-02-04 15:45:28 +0900

    feat: 유저 정보 가져오기 기능 개발

--- src/main/java/com/neodo/neodo_backend/users/controller/UserController.java
+++ src/main/java/com/neodo/neodo_backend/users/controller/UserController.java
@@ -3,16 +3,11 @@ package com.neodo.neodo_backend.users.controller;
 import com.neodo.neodo_backend.users.dto.request.UserCreateRequest;
 import com.neodo.neodo_backend.users.dto.response.UserResponse;
 import com.neodo.neodo_backend.users.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -41,5 +36,13 @@ public class UserController {
         UserResponse response = userService.signup(request);
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/my-page")
+    public ResponseEntity<?> get(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        UserResponse response = userService.get(userDetails.getUser());
+        return ResponseEntity.ok(response);
+    }
+
+
 
 }
