acdfa53cfe0bc5b93b7faf29eed5b86f1541964e
kim <202204266@hufs.ac.kr>
2025-02-15 13:24:48 +0900

    refactor/회원가입/#14

--- src/main/java/com/neodo/neodo_backend/users/dto/response/UserResponse.java
+++ src/main/java/com/neodo/neodo_backend/users/dto/response/UserResponse.java
@@ -8,17 +8,17 @@ import lombok.Setter;
 @Setter
 public class UserResponse {
 
-    private Long id;
+    private Long userId;
     private String username;
     private String email;
 
-    private UserResponse(Long id, String username, String email) {
-        this.id = id;
+    private UserResponse(Long userId, String username, String email) {
+        this.userId = userId;
         this.username = username;
         this.email = email;
     }
 
     public static UserResponse from(UserEntity user){
-        return new UserResponse(user.getId(), user.getUsername(), user.getEmail());
+        return new UserResponse(user.getUserId(), user.getUsername(), user.getEmail());
     }
 }
