3eeb00906adb65bfa6880204f9bc924becdc7f8a
kim <202204266@hufs.ac.kr>
2025-02-18 15:19:12 +0900

    feat/스피치 보드 제목 수정/#8

---
+++ src/main/java/com/neodo/neodo_backend/speechBoards/infrastructure/SpeechBoardRepositoryImpl.java
@@ -0,0 +1,22 @@
+package com.neodo.neodo_backend.speechBoards.infrastructure;
+
+import com.neodo.neodo_backend.speechBoards.infrastructure.entity.SpeechBoardEntity;
+import com.neodo.neodo_backend.speechBoards.service.SpeechBoardRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class SpeechBoardRepositoryImpl implements SpeechBoardRepository {
+
+    private final SpeechBoardJpaRepository speechBoardJpaRepository;
+
+    public SpeechBoardRepositoryImpl(SpeechBoardJpaRepository speechBoardJpaRepository){
+        this.speechBoardJpaRepository = speechBoardJpaRepository;
+    }
+
+    public Optional<SpeechBoardEntity> findById(Long speechBoardId) {
+        return speechBoardJpaRepository.findById(speechBoardId);
+    }
+
+}
---
+++ src/main/java/com/neodo/neodo_backend/speechBoards/service/SpeechBoardRepository.java
@@ -0,0 +1,9 @@
+package com.neodo.neodo_backend.speechBoards.service;
+
+import com.neodo.neodo_backend.speechBoards.infrastructure.entity.SpeechBoardEntity;
+
+import java.util.Optional;
+
+public interface SpeechBoardRepository {
+    Optional<SpeechBoardEntity> findById(Long speechBoardId);
+}
