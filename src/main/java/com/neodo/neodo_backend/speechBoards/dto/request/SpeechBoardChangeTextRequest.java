f191310cc2d5e2c5442580787644a1341e400dd6
kim <202204266@hufs.ac.kr>
2025-02-19 17:32:34 +0900

    feat/스피치 보드 텍스트 수정/#19

--- src/main/java/com/neodo/neodo_backend/speechBoards/dto/request/SpeechBoardChangeTextRequest.java
+++ src/main/java/com/neodo/neodo_backend/speechBoards/dto/request/SpeechBoardChangeTextRequest.java
@@ -3,10 +3,8 @@ package com.neodo.neodo_backend.speechBoards.dto.request;
 import com.neodo.neodo_backend.common.constant.ValidationMessage;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class SpeechBoardChangeTextRequest {
 
     @NotBlank(message = ValidationMessage.RESPONSE_NOT_BLANK)
