a27d23e45a025e9dac1ea1efd2d929be101876d9
kim <202204266@hufs.ac.kr>
2025-02-20 22:22:47 +0900

    refactor: 스피치 보드 부분 삭제

--- src/main/java/com/neodo/neodo_backend/speechBoards/service/SpeechBoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.neodo.neodo_backend.speechBoards.service;
-
-import com.neodo.neodo_backend.speechBoards.infrastructure.entity.SpeechBoardEntity;
-
-import java.util.Optional;
-
-public interface SpeechBoardRepository {
-    Optional<SpeechBoardEntity> findById(Long speechBoardId);
-}
--- src/main/java/com/neodo/neodo_backend/speechBoards/service/SpeechBoardService.java
+++
@@ -1,7 +0,0 @@
-package com.neodo.neodo_backend.speechBoards.service;
-
-import com.neodo.neodo_backend.speechBoards.dto.request.SpeechBoardChangeTitleRequest;
-
-public interface SpeechBoardService {
-    void speechBoardChangeTitle(Long speechBoardId,SpeechBoardChangeTitleRequest request);
-}
