87823233d064d12dfa3dfec672af031023bf564c
geneeuchoi <chyj2108@naver.com>
2025-02-21 22:05:36 +0900

    refactor: dto 필드 변경

--- src/main/java/com/neodo/neodo_backend/speechBoard/dto/response/SpeechBoardResponse.java
+++ src/main/java/com/neodo/neodo_backend/speechBoard/dto/response/SpeechBoardResponse.java
@@ -4,20 +4,22 @@ import com.neodo.neodo_backend.speechBoard.infrastructure.entity.SpeechBoardEnti
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class SpeechBoardResponse {
     private Long id;
     private Long userId;
     private String title;
-    private String record;
+    private LocalDateTime createdAt;
 
     public static SpeechBoardResponse from(SpeechBoardEntity speechBoard) {
         return SpeechBoardResponse.builder()
                 .id(speechBoard.getId())
                 .userId(speechBoard.getUser().getId())
                 .title(speechBoard.getTitle())
-                .record(speechBoard.getRecord())
+                .createdAt(speechBoard.getCreatedAt())
                 .build();
     }
 }
