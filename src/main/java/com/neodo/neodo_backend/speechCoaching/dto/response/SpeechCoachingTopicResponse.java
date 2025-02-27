df6cd155304b8f348622dce0d63b767080c6c7ed
geneeuchoi <chyj2108@naver.com>
2025-02-27 13:58:50 +0900

    refactor: 토픽이 아이디와 함께 반환될 수 있도록 변경

--- src/main/java/com/neodo/neodo_backend/speechCoaching/dto/response/SpeechCoachingTopicResponse.java
+++ src/main/java/com/neodo/neodo_backend/speechCoaching/dto/response/SpeechCoachingTopicResponse.java
@@ -1,6 +1,7 @@
 package com.neodo.neodo_backend.speechCoaching.dto.response;
 
 import com.neodo.neodo_backend.speechBoard.infrastructure.entity.SpeechBoardEntity;
+import com.neodo.neodo_backend.topic.dto.response.TopicResponse;
 import com.neodo.neodo_backend.topic.infrastructure.entity.TopicEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,13 +18,15 @@ import java.util.stream.Collectors;
 public class SpeechCoachingTopicResponse {
     private Long speechBoardId;
     private String title;
-    private List<String> topics;
+    private List<TopicResponse> topics;
 
     public static SpeechCoachingTopicResponse from(SpeechBoardEntity speechBoard, List<TopicEntity> topics) {
         return SpeechCoachingTopicResponse.builder()
                 .speechBoardId(speechBoard.getId())
                 .title(speechBoard.getTitle())
-                .topics(topics.stream().map(TopicEntity::getTopic).collect(Collectors.toList()))
+                .topics(topics.stream()
+                        .map(TopicResponse::from)
+                        .collect(Collectors.toList()))
                 .build();
     }
 }
